296d12dc81ccf62d463688b6187e3db4bf68f81f
Ignitius <kasoigoi@gmail.com>
2026-07-12 15:41:42 +0300

    Add TodoViewSet and RegisterView

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from django.contrib.auth.models import User
+from .models import Todo
+from .serializers import TodoSerializer, Re
+gisterSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user).order_by('due_date')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
