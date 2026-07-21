24978882c75a7438af59ad7a4e20ff4def3e132e
Ignitius <kasoigoi@gmail.com>
2026-07-21 18:43:58 +0300

    Point frontend at live Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://planify-tgoq.onrender.com/api/',
 });
 
 const PUBLIC_ENDPOINTS = ['register/', 'token/', 'token/refresh/'];
